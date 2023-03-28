69f3348094cc98204c8b015c69ac65d9b6f8d8c9
HotPotTest <hotpottest1@gmail.com>
2023-03-28 15:19:57 +0530

    user signup basic

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const jwt = require('jsonwebtoken');
 
 /*exports.signup = async (req, res,next) => {
   try {
