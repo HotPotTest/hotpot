d82af131e445400cb3f648237cf00e9def7b1d72
omkar randive <38181204+randiveomkar95@users.noreply.github.com>
2023-04-16 07:37:06 +0530

    ligin register cors error

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,6 +14,8 @@ const signToken = (id) => {
 };
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.status(statusCode).json({
     status: 'success',
     token,
