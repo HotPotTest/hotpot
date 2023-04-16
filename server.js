c98fb8c3318306c661714c5fab74ca95077a8180
omkar randive <38181204+randiveomkar95@users.noreply.github.com>
2023-04-16 07:49:28 +0530

    env port added

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ mongoose
     console.log('db connection successful ');
   });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`app running on port ${port}`);
 });
