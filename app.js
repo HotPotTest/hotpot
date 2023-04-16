344c4a8dd6720c6eb7c07117e1879b1bed8f80d0
HotPotTest <hotpottest1@gmail.com>
2023-04-16 09:07:11 +0530

    cors

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
-
+var cors = require('cors');
 //slide 222
 const compression = require('compression');
 
@@ -27,6 +27,8 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
+
+app.use(cors());
 
 app.use((req, res, next) => {
   // console.log('hello from the middle ware 🏀 ');
